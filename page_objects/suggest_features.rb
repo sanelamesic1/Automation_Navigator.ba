dc8d7e16b63119a91f003c8dda48fd9b8cbccf32
sanelamesic1 <sane_mesic@hotmail.com>
2019-07-17 16:17:13 +0200

    Formatting code

--- page_objects/suggest_features.rb
+++ page_objects/suggest_features.rb
@@ -23,50 +23,49 @@ class SuggestFeatures < HomePage
     @browser.button(class: ["btn", "green-button"])
   end
   def send_message 
-  	@browser.div(class: ["alert", "alert-success"])
+    @browser.div(class: ["alert", "alert-success"])
   end
   def home_button 
     @browser.a(href: "#/categories")
   end
   def odustani_button
-  	@browser.button(class: ["btn", "grey-button"])
+    @browser.button(class: ["btn", "grey-button"])
   end
 
 	#akcije
-
-	def click_suggest_features_button
-		suggest_features_button.click
-	end
-	def add_name (name)
-		name_field.set name
-	end
-	def add_email (email)
-		email_field.set email
-	end
-	def add_comment (comment)
-		comment_field.set comment
-	end
-	def click_pohvala_button
-		pohvala_button.radio.set
-	end
-	def click_kritika_button
-		kritika_button.radio.set
-	end
-	def click_posalji_button
-		posalji_button.click
-	end
-	def check_suggest_send? (sendmessage)
+  def click_suggest_features_button
+    suggest_features_button.click
+  end
+  def add_name (name)
+    name_field.set name
+  end
+  def add_email (email)
+    email_field.set email
+  end
+  def add_comment (comment)
+    comment_field.set comment
+  end
+  def click_pohvala_button
+    pohvala_button.radio.set
+  end
+  def click_kritika_button
+    kritika_button.radio.set
+  end
+  def click_posalji_button
+    posalji_button.click
+  end
+  def check_suggest_send? (sendmessage)
     send_message.exists?
     send_message.text.eql? ("#{sendmessage}")
   end
   def check_suggest_not_send?
-  	send_message.nil?
+    send_message.nil?
   end
   def click_home_button
     home_button.click
   end
   def click_odustani_button
-  	odustani_button.click
+    dustani_button.click
   end
   def form_closed? (browser_url)
     @browser.url.eql?("#{browser_url}")

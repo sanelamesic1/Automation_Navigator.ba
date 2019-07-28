94d2bb3593572ebb48945842f23ba97a3451d79b
sanelamesic1 <sane_mesic@hotmail.com>
2019-07-28 15:04:49 +0200

    Regression_tests

--- spec/helper.rb
+++ spec/helper.rb
@@ -9,11 +9,11 @@ rescue
 end
 
 RSpec.configure do |config|
-	config.before (:all) {
-		@browser = Watir::Browser.new :chrome
-		@main = HomePage.new(@browser)
-		@main.goto_navigator
-	}
-	config.after(:all) {@browser.close}
+  config.before (:all) {
+    @browser = Watir::Browser.new :chrome
+    @main = HomePage.new(@browser)
+    @main.goto_navigator
+  }
+  config.after(:all) {@browser.close}
 end
 
